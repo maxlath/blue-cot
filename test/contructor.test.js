db176578b5135ae3906988b36f7eb012803486b5
maxlath <k@maxlath.eu>
2017-01-14 22:00:55 +0100

    caching db objects when possible to avoid creating new objects when possible

--- test/contructor.test.js
+++ test/contructor.test.js
@@ -1,4 +1,4 @@
-require('should')
+const should = require('should')
 const cot = require('../lib/cot')
 
 describe('cot', function () {
@@ -18,6 +18,19 @@ describe('cot', function () {
     withViewFunctions.forEach(function (fnName) {
       db[fnName].should.be.a.Function()
     })
+  })
+
+  it('should cache db objects with identic arguments', function () {
+    const db = cot({ port: 80, hostname: 'foo' })('bar')
+    const db2 = cot({ port: 80, hostname: 'foo' })('bar')
+    const db3 = cot({ port: 80, hostname: 'foo' })('bar', 'buzz')
+    const db4 = cot({ port: 80, hostname: 'foo' })('bar', 'boudu')
+    const db5 = cot({ port: 80, hostname: 'foo' })('bar', 'boudu')
+    should(db === db2).be.true()
+    should(db !== db3).be.true()
+    should(db !== db4).be.true()
+    should(db3 !== db4).be.true()
+    should(db4 === db5).be.true()
   })
 })
 
