3688387a340f44286c37df705e19b44c96b3e31e
maxlath <k@maxlath.eu>
2017-01-15 11:41:44 +0100

    Revert "caching db objects when possible to avoid creating new objects when possible" Caching should be the responsibility of the user, who for some reasons might want different instances of the API It's a breaking change but it wasn't a documented feature so no need to bump the major version
    This reverts commit db176578b5135ae3906988b36f7eb012803486b5.

--- test/contructor.test.js
+++ test/contructor.test.js
@@ -1,4 +1,4 @@
-const should = require('should')
+require('should')
 const cot = require('../lib/cot')
 
 describe('cot', function () {
@@ -19,19 +19,6 @@ describe('cot', function () {
     dbHanlderFunctionsWithViewFunctions.forEach(function (fnName) {
       db[fnName].should.be.a.Function()
     })
-  })
-
-  it('should cache db objects with identic arguments', function () {
-    const db = cot({ port: 80, hostname: 'foo' })('bar')
-    const db2 = cot({ port: 80, hostname: 'foo' })('bar')
-    const db3 = cot({ port: 80, hostname: 'foo' })('bar', 'buzz')
-    const db4 = cot({ port: 80, hostname: 'foo' })('bar', 'boudu')
-    const db5 = cot({ port: 80, hostname: 'foo' })('bar', 'boudu')
-    should(db === db2).be.true()
-    should(db !== db3).be.true()
-    should(db !== db4).be.true()
-    should(db3 !== db4).be.true()
-    should(db4 === db5).be.true()
   })
 })
 
