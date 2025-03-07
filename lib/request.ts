9c11d43e05bf1d91a7550482410ce35fc32a5026
maxlath <k@maxlath.eu>
2025-03-07 16:21:26 +0100

    request: retry on ERR_STREAM_PREMATURE_CLOSE

--- lib/request.ts
+++ lib/request.ts
@@ -8,13 +8,16 @@ const tryRequest = async (url, options, attempt = 1) => {
   try {
     return await fetch(url, options)
   } catch (err) {
-    // Retry  on 'socket hangup' errors, as this might be due to a persistant connection
-    // (agent with keepAlive=true) that was closed because of another error, so retrying
-    // should give us access to the primary error
-    if (err.code === 'ECONNRESET' && attempt < 20) {
+    // Retry on
+    // - ECONNRESET aka 'socket hangup' errors, as this might be due to a persistant connection
+    //   (agent with keepAlive=true) that was closed because of another error, so retrying
+    //   should give us access to the primary error
+    // - ERR_STREAM_PREMATURE_CLOSE: thrown by node-fetch. It can happen when the maxSockets limit is reached.
+    //   See https://github.com/node-fetch/node-fetch/issues/1576#issuecomment-1694418865
+    if ((err.code === 'ECONNRESET' || err.code === 'ERR_STREAM_PREMATURE_CLOSE') && attempt < 20) {
       const delayBeforeRetry = 500 * attempt ** 2
       await sleep(delayBeforeRetry)
-      console.warn(`[blue-cot retrying after ECONNRESET (attempt: ${attempt})]`, err)
+      console.warn(`[blue-cot retrying after ${err.code} (attempt: ${attempt})]`, err)
       return tryRequest(url, options, ++attempt)
     } else {
       throw err
