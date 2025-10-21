4d0c4be04009a93be1d79043a002076fa534693f
maxlath <k@maxlath.eu>
2025-10-21 23:54:00 +0200

    dependencies: upgrade eslint to v9

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   'node-option': [
-    'loader=ts-node/esm',
+    'import=tsx/esm',
     // Mute node error: (node:29544) ExperimentalWarning: `--experimental-loader` may be removed in the future; instead use `register()`
     'no-warnings',
   ],
